faa678fa597957337808537618b4701f050d608c
bigceeXR <bigceexr@gmail.com>
2026-05-29 18:27:22 +0100

    Delete js directory

--- js/config.js
+++
@@ -1,4 +0,0 @@
-// Replace with your actual keys from Supabase & Paystack Dashboard
-export const SUPABASE_URL = 'https://your-project.supabase.co';
-export const SUPABASE_ANON_KEY = 'your-anon-key';
-export const PAYSTACK_PUBLIC_KEY = 'pk_live_or_test_xxxxxxxxx';
