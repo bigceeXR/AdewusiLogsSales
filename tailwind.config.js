151f79363405f2f5239ad3c8a6c6761698d5e242
bigceeXR <bigceexr@gmail.com>
2026-05-29 15:30:25 +0100

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        brand: {
+          light: '#f4f7ff',
+          DEFAULT: '#2563eb',
+          dark: '#1e3a8a',
+          navy: '#0f172a'
+        }
+      }
+    },
+  },
+  plugins: [],
+}
